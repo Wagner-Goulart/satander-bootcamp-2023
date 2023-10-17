11dfdec140edea03f76427e602a718955557471a
Wagner <wagnergoulart0@gmail.com>
2023-10-17 19:38:30 -0300

    first commit

---
+++ src/test/java/edu/java/SatanderBootcamp2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SatanderBootcamp2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
